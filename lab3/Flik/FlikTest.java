4226c5d8343eb4a13891ba04efaf87e943f3f940
wqq <qiqi,wu@xyzrobotics.ai>
2020-10-04 18:10:37 +0800

    finish Lab3

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlikTest {
+
+    @Test
+    public void testisSameNumber() {
+        assertEquals(true, Flik.isSameNumber(128, 128));
+    }
+
+}
