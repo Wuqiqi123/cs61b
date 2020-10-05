1b1d94191c26457adcad89eca98c1c3ebf508c93
wqq <qiqi,wu@xyzrobotics.ai>
2020-10-05 12:32:43 +0800

    using one sentinel

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,6 +1,5 @@
-
-
-
+import java.lang.module.FindException;
+import java.time.temporal.Temporal;
 
 public class LinkedListDeque<T> {
 
@@ -15,32 +14,30 @@ public class LinkedListDeque<T> {
         }
     }
 
-    private final Node sentinelFront;
-    private final Node sentinelBack;
+    private final Node sentinel;
     private int size;
 
     public LinkedListDeque(){
-        sentinelFront = new Node(null, null, null);
-        sentinelBack = new Node(null, null, null);
-        sentinelFront.next = sentinelBack;
-        sentinelBack.prev = sentinelFront;
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
         size = 0;
     }
 
     /** Adds an item of type T to the front of the deque.*/
     public void addFirst(T item) {
-        Node node = new Node(sentinelFront, item,  sentinelFront.next);
-        sentinelFront.next.prev = node;
-        sentinelFront.next = node;
-        size += 1;
+        Node newNode = new Node(sentinel, item, sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size++;
     }
 
     /* Adds an item of type T to the back of the deque*/
     public void addLast(T item) {
-        Node node = new Node(sentinelBack.prev, item,  sentinelBack);
-        sentinelBack.prev.next = node;
-        sentinelBack.prev = node;
-        size += 1;
+        Node newNode = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size++;
     }
 
     /** Returns true if deque is empty, false otherwise.*/
@@ -53,50 +50,46 @@ public class LinkedListDeque<T> {
     }
 
     public T removeFirst() {
-        if(sentinelFront.next == sentinelBack) {
-           return null;
+        if(size == 0){
+            return null;
         }
-        Node removedNode = sentinelFront.next;
-        sentinelFront.next = removedNode.next;
-        removedNode.next.prev = sentinelFront;
+        Node removedNode = sentinel.next;
+        sentinel.next = removedNode.next;
+        removedNode.next.prev = sentinel;
         size--;
         return removedNode.item;
     }
 
     public T removeLast() {
-        if(sentinelBack.prev == sentinelFront) {
+        if(size == 0){
             return null;
         }
-        Node removedNode = sentinelBack.prev;
-        sentinelBack.prev = removedNode.prev;
-        removedNode.prev.next = sentinelBack;
+        Node removedNode = sentinel.prev;
+        sentinel.prev = removedNode.prev;
+        removedNode.prev.next = sentinel;
         size--;
         return removedNode.item;
-
     }
 
     public T get(int index){
-        int i= 0;
-        Node temp = sentinelFront.next;
-        while(i<index){
-            if (temp == sentinelBack){
-                break;
-            }
-            temp = temp.next;
-            i++;
+        if (index >= size) {
+            return null;
         }
-        return (temp == sentinelBack) ? null : temp.item;
+        Node temp = sentinel.next;
+        for(int i =0; i< index; i++) {
+            temp = temp.next;
+        }
+        return temp.item;
     }
 
     public void printDeque() {
-        for(int i=0;i<size; i++) {
-            if (i == size - 1){
-                System.out.println(get(i));
+        Node temp  = sentinel.next;
+        for(int i =0; i< size; i++) {
+            if (i == size - 1) {
+                System.out.println(temp.item);
             }
-            else {
-                System.out.print(get(i) + " ");
-            }
-
+            System.out.print(temp.item + " ");
+            temp = temp.next;
         }
     }
 }
